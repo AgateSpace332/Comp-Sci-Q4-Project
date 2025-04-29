8db5d335fc6a1ff09bbe155d958260895ecb9169
AgateSpace332 <bchelnitsky27@belmontschools.net>
2025-04-29 13:29:22 -0400

    Update wordguessinggame.js

--- wordguessinggame.js
+++ wordguessinggame.js
@@ -1,1 +1,51 @@
+let word = "";
+let attempts = 6;
+let guessedLetters = [];
+const words = ["happy", "paper", "letter", "water", "dress", "plant", "house", "music", "chair", "strong", "write", "small", "travel", "green"];
 
+function startGame() {
+    word = words[Math.floor(Math.random() * words.length)];
+    guessedLetters = [];
+    attempts = 6;
+    document.getElementById("attempts").textContent = attempts;
+    displayWord();
+    displayAlphabet();
+}
+
+function displayWord() {
+    let wordDisplay = word.split('').map(letter => guessedLetters.includes(letter) ? letter : '_').join(' ');
+    document.getElementById('word').textContent = wordDisplay;
+}
+
+function displayAlphabet() {
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    let alphabetDiv = document.getElementById('alphabet');
+    alphabetDiv.innerHTML = '';
+    for (let i = 0; i < alphabet.length; i++) {
+        let letter = alphabet[i];
+        let letterButton = document.createElement('span');
+        letterButton.textContent = letter;
+        letterButton.classList.add('letter');
+        letterButton.onclick = () => guessLetter(letter, letterButton);
+        alphabetDiv.appendChild(letterButton);
+    }
+}
+
+function guessLetter(letter, button) {
+    button.classList.add('clicked');
+    if (word.includes(letter)) {
+        guessedLetters.push(letter);
+    } else {
+        attempts--;
+        document.getElementById("attempts").textContent = attempts;
+    }
+    displayWord();
+
+    if (attempts === 0) {
+        alert("Game Over! The word was: " + word);
+    } else if (!word.split('').some(letter => !guessedLetters.includes(letter))) {
+        alert("You Win! The word was: " + word);
+    }
+}
+
+startGame();
